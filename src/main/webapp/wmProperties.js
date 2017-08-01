7f032c8aa75dc248eccc143f114d53355dfff1fb
Vishnu Alla <vishnu.alla@wavemaker.com>
2017-08-01 07:09:56 +0000

    ser Files modified - [wmProperties.js]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -1,13 +1,13 @@
 var _WM_APP_PROPERTIES = {
-  "activeTheme" : "neon",
-  "dateFormat" : "",
-  "defaultLanguage" : "en",
-  "displayName" : "DublinUInew",
-  "homePage" : "Main",
-  "name" : "DublinUInew",
-  "platformType" : "WEB",
-  "supportedLanguages" : "en",
-  "timeFormat" : "",
-  "type" : "APPLICATION",
-  "version" : "15.0"
+    "activeTheme": "neon",
+    "dateFormat": "",
+    "defaultLanguage": "en",
+    "displayName": "DublinUInew",
+    "homePage": "Main",
+    "name": "DublinUInew",
+    "platformType": "WEB",
+    "supportedLanguages": "en",
+    "timeFormat": "",
+    "type": "APPLICATION",
+    "version": "16.0"
 };
